79e0ddbeae7c54b5789e6286289638605836eacc
unproZhai <zhaix15@gmail.com>
2021-12-29 23:38:51 -0500

    Update keys and info

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb://logan:password1@ds135993.mlab.com:35993/randomongodb1',
+  // mongoURI: 'mongodb://logan:password1@ds135993.mlab.com:35993/randomongodb1',
+  mongoURI: 'mongodb+srv://tester:uzD67Un85CPNJWp6@cluster0.1eoua.mongodb.net/Cluster0?retryWrites=true&w=majority',
   secretOrKey: 'secret'
 };
